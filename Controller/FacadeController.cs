67470606eae34b5df3fcf0bf160f4aaf00cec39c
colman79 <colman_no1@hotmail.com>
2014-05-08 09:25:55 +0200

    asd

--- Controller/FacadeController.cs
+++ Controller/FacadeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using Model;
@@ -28,7 +29,7 @@ namespace Controller
 
         public void BLaBLaBLa()
         {
-            
+            int testint = 0;
             //hello
         }
 
