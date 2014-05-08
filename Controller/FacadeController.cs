7c8b820541cc74ba14ed7476777acefac1b42b8e
janek12370 <janek12370@gmail.com>
2014-05-08 09:36:23 +0200

    Revert "htd"
    This reverts commit 6c69b6d67870c5d6d322706341b6e8f50be12190.

--- Controller/FacadeController.cs
+++ Controller/FacadeController.cs
@@ -29,16 +29,7 @@ namespace Controller
 
         public void BLaBLaBLa()
         {
-<<<<<<< HEAD
             int testint = 0;
-=======
-            List<string> customerList = new List<string>();
-            foreach (Customer c in MyCustomerCollection.MyCustomers)
-            {
-                customerList.Add(c.Name);
-            }
-            return customerList;
->>>>>>> 03ae7bfbd806332d276699bb157cbbbb0d63bff9
             //hello
         }
 
@@ -51,7 +42,6 @@ namespace Controller
             }
             return customerList;
         }
-
         public List<string> GetBeers()
         {
             List<string> beerList = new List<string>();
@@ -61,6 +51,8 @@ namespace Controller
             }
             return beerList;
         }
+
+       
 
         public void CreateDeliveryNote()  
         {
